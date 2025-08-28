bd85b3437587cd17aba18572c8d30b702515d153
Hankaji <hankaji135@gmail.com>
2025-08-28 11:39:43 +0700

    refactor: rearrange endpoints of flight to entity-group

--- routes/flight_routes.go
+++ routes/flight_routes.go
@@ -18,15 +18,17 @@ func RegisterFlightRoutes(r *gin.RouterGroup) {
 		},
 	}
 
+	// Flights
 	r.GET("flights", controller.GetFlights)
 	r.GET("flights/:flightID", controller.GetFlightsByID)
-	r.GET("flights/:flightID/seats", controller.GetSeatAvailability)
-	r.GET("flights/:flightID/tickets", controller.GetTickets)
-
 	r.POST("flights", controller.CreateFlight)
-	r.POST("flights/:flightID/tickets", controller.CreateTicket)
-
-	r.PATCH("flights/:flightID/tickets/:ticketID", controller.UpdateTicket)
-
 	r.DELETE("flights/:flightID", controller.DeleteFlight)
+
+	// Seats
+	r.GET("flights/:flightID/seats", controller.GetSeatAvailability)
+
+	// Tickets
+	r.GET("flights/:flightID/tickets", controller.GetTickets)
+	r.POST("flights/:flightID/tickets", controller.CreateTicket)
+	r.PATCH("flights/:flightID/tickets/:ticketID", controller.UpdateTicket)
 }
